e7445cba790572c08c0443e7225a8fa322625138
Maya Dahlke <mayadahlke@yahoo.com>
2021-01-28 15:50:26 -0800

    add blog post app

---
+++ tutorialapp/blogapp/src/PostForm.js
@@ -0,0 +1,53 @@
+import React, {useState} from 'react';
+import {Form, Button} from 'react-bootstrap';
+
+function PostForm(props) {
+    const {addPostToList} = props;
+    const [checked, setChecked] = useState(false);
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    
+    function handleSubmit(event) {
+        event.preventDefualt();
+        addPostToList({id: String(Math.random() * 1000 + 100), title, body});
+        setTitle('');
+        setBody('');
+        setChecked(false);
+    }
+    
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Form.Check 
+                type='checkbox' 
+                label='Add Post' 
+                checked={checked} 
+                onChange={() => setChecked((prevState) => !prevState)}
+            />
+            {checked && (
+                <>
+                    <Form.Group>
+                        <Form.Control 
+                            type='input' 
+                            placeholder='Title' 
+                            value={title}
+                            onChange={(event) => setTitle(event.target.value)}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Post Body</Form.Label>
+                        <Form.Control 
+                            as='textarea' 
+                            rows={10} 
+                            value={body}
+                            onChange={(event) => setBody(event.target.value)}
+                        />
+                    </Form.Group>
+                    <Button variant='primary' type='submit'>Submit</Button>
+                </>
+            )}
+            <hr></hr>
+        </Form>
+    ); 
+}
+
+export default PostForm;
