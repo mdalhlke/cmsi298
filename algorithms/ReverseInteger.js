a1f829ea9211c309cbbdf6f5a19c1a8c5252cde6
Maya Dahlke <mayadahlke@yahoo.com>
2021-01-28 16:33:00 -0800

    add week2 algo

---
+++ algorithms/ReverseInteger.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let strx = x.toString();
+    let arr = strx.split('');
+    let isNeg = false;
+    
+    if (arr[0] == '-') {
+        isNeg = true;
+        arr.shift();
+    }
+    
+    if (arr.length > 1) {
+    	while (arr[arr.length - 1] == '0') {
+            arr.pop();
+    	}
+	}
+
+    let res = arr.reverse().join('');
+
+    if (res >= (Math.pow(2,31) - 1)) {
+    	return 0;
+    }
+      
+    if (isNeg) {  
+       return -1 * parseInt(res);
+    } else {
+    	return parseInt(res);
+    }
+             
+};
