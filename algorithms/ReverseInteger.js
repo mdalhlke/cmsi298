5072f6c92ab586d16c93cc284124fb440af10aa3
Maya Dahlke <mayadahlke@yahoo.com>
2021-03-12 15:03:34 -0800

    add algo

--- algorithms/ReverseInteger.js
+++ algorithms/ReverseInteger.js
@@ -2,32 +2,31 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    let strx = x.toString();
-    let arr = strx.split('');
-    let isNeg = false;
-    
-    if (arr[0] == '-') {
-        isNeg = true;
-        arr.shift();
-    }
-    
-    if (arr.length > 1) {
-    	while (arr[arr.length - 1] == '0') {
-            arr.pop();
-    	}
-	}
+var reverse = function (x) {
+  let strx = x.toString();
+  let arr = strx.split('');
+  let isNeg = false;
 
-    let res = arr.reverse().join('');
+  if (arr[0] == '-') {
+    isNeg = true;
+    arr.shift();
+  }
 
-    if (res >= (Math.pow(2,31) - 1)) {
-    	return 0;
+  if (arr.length > 1) {
+    while (arr[arr.length - 1] == '0') {
+      arr.pop();
     }
-      
-    if (isNeg) {  
-       return -1 * parseInt(res);
-    } else {
-    	return parseInt(res);
-    }
-             
+  }
+
+  let res = arr.reverse().join('');
+
+  if (res >= Math.pow(2, 31) - 1) {
+    return 0;
+  }
+
+  if (isNeg) {
+    return -1 * parseInt(res);
+  } else {
+    return parseInt(res);
+  }
 };
